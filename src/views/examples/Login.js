b9b9a319d61da6ca3ea94dfcd7031ad2ef1f619b
KasaSandhya <svanapalli@baylogictech.com>
2021-01-27 10:44:52 +0530

    changed login functionality

--- src/views/examples/Login.js
+++ src/views/examples/Login.js
@@ -37,14 +37,89 @@ import {
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import CardsFooter from "components/Footers/CardsFooter.js";
+import loginUser from 'strapi/loginUser';
+import registerUser from 'strapi/registerUser';
+import { useHistory } from 'react-router-dom';
+import { UserContext } from 'context/user';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      setEmail: '',
+      password: '',
+      setPassword: '',
+      username: 'default',
+      setUsername: 'default',
+      isMember: true,
+      setIsMember: true
+    };
+  }
+
+  emailHandler = (event) => {
+    this.setState({email: event.target.value});
+  }
+
+  passwordHandler = (event) => {
+    this.setState({password: event.target.value});
+  }
+
+  static contextType  = UserContext;
   componentDidMount() {
     document.documentElement.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
     this.refs.main.scrollTop = 0;
   }
   render() {
+    const {userLogin, alert, showAlert} = this.context;
+    const { history } = this.props;
+    const { email, setEmail, password, setPassword, username, setUsername, isMember, setIsMember} = this.state;
+
+    let isEmpty = !email || !password || !username || alert.show;
+
+    const toggleMember = () => {
+      setIsMember((prevMember) => {
+        // console.log(prevMember);
+        let isMember = !prevMember;
+        isMember ? setUsername('default') : setUsername('');
+
+        return isMember;
+      });
+    };
+
+    const handleSubmit = async (e) => {
+      showAlert({ msg: 'accessing user data. please wait...' });
+      e.preventDefault();
+  
+      let response;
+  
+      if (isMember) {
+        response = await loginUser( email, password );
+      } else {
+        response = await registerUser( email, password, username);
+      }
+  
+      if (response) {
+        const {
+          jwt: token,
+          user: { username },
+        } = response.data;
+  
+        const newUser = { token, username };
+  
+        userLogin(newUser);
+        showAlert({ msg: `you are logged in ${username}. shop away my friend` });
+  
+        history.push('/');
+      } else {
+        showAlert({
+          msg: 'there was an error. please try again...',
+          type: 'danger',
+        });
+        // show alert
+      }
+    };
     return (
       <>
         <DemoNavbar />
@@ -111,7 +186,9 @@ class Login extends React.Component {
                                 <i className="ni ni-email-83" />
                               </InputGroupText>
                             </InputGroupAddon>
-                            <Input placeholder="Email" type="email" />
+                            <Input placeholder="Email" type="email" id="email" value={email}
+                            onChange={this.emailHandler}
+                            /> 
                           </InputGroup>
                         </FormGroup>
                         <FormGroup>
@@ -124,7 +201,9 @@ class Login extends React.Component {
                             <Input
                               placeholder="Password"
                               type="password"
-                              autoComplete="off"
+                              id="password"
+                              value={password}
+                              onChange={this.passwordHandler}
                             />
                           </InputGroup>
                         </FormGroup>
@@ -146,10 +225,17 @@ class Login extends React.Component {
                             className="my-4"
                             color="primary"
                             type="button"
+                            onClick={handleSubmit}
                           >
                             Sign in
                           </Button>
                         </div>
+                        <p className="register-link">
+                        {isMember ? 'need to register' : 'already a member'}
+                        <button type="button" onClick={toggleMember}>
+                          click here
+                        </button>
+                      </p>
                       </Form>
                     </CardBody>
                   </Card>
