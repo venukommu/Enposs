903866c2649aec607a767fc60593c67fecacd202
madhulikagadang <gadangmadhulika@gmail.com>
2021-03-02 15:03:18 +0530

    logo fixed in responsice view

--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -95,7 +95,7 @@ class DemoNavbar extends React.Component {
                       <Link to="/">
                         <img
                           alt="..."
-                          src={require("assets/img/brand/argon-react.png")}
+                          src={require("assets/img/brand/logo.png")}
                         />
                       </Link>
                     </Col>
