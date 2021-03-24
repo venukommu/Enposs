033b3a7a2e07baeca4dab6abfcb52a9bc4d85b50
madhulikagadang <gadangmadhulika@gmail.com>
2021-03-24 16:58:44 +0530

    chnage

--- src/views/examples/InstallForce.js
+++ src/views/examples/InstallForce.js
@@ -22,6 +22,8 @@ import {
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import CardsFooter from "components/Footers/CardsFooter.js";
+import CardTitle from "reactstrap/lib/CardTitle";
+import CardText from "reactstrap/lib/CardText";
 //import { appConfig } from "services/config.js";
 
 // index page sections
@@ -156,21 +158,22 @@ class InstallForce extends React.Component {
             <Container>
               <Row className="justify-content-center">
                 <Col lg="12">
-                  <Card className="bg-gradient-white">
+                 {/*} <Card className="bg-gradient-white">
                   <h4 className="display-3 font-weight-bold text-primary">
-                          {/*}    {companystory.Title}*/}
+                       
                           How To Install
                       </h4>
                   <CardBody>
                     <CardImg alt="..."
                       src={`${require("assets/img/theme/Installation1.PNG")}`}/>
+                      <CardText />
                   </CardBody>
                   <CardBody>
                     <CardImg alt="..."
                       src={`${require("assets/img/theme/installation2.PNG")}`}/>
                   </CardBody>
                   
-                  </Card>
+    </Card>*/}
                   {/*<Card className="bg-gradient-info text-white">
                     <CardBody>
                     <h4>Safety measures</h4>
@@ -206,7 +209,7 @@ class InstallForce extends React.Component {
                     href="#pablo"
                     role="tab"
                   >
-                    Safety measures
+                   1.Installation of FORCE
                   </NavLink>
                 </NavItem>
                 <NavItem>
@@ -219,31 +222,68 @@ class InstallForce extends React.Component {
                     href="#pablo"
                     role="tab"
                   >
-                    Steps of installation
+                    2.Installation of FORCE
                   </NavLink>
                 </NavItem>
               </Nav>
             </div>
             <Card className="shadow">
               <CardBody>
+              <Row >
+                <Col lg="6">
                 <TabContent activeTab={"plainTabs" + this.state.plainTabs}>
                   <TabPane tabId="plainTabs1">
-                    <ul>
-                      <li>Should confirm the main circuit breaker is open.</li>
-                      <li>Conform the rate voltage and capacity of FORCE.</li>
+                
+                  <ul>
+                      <p className="text-danger text-right font-weight-bold">* Always consult a Licensed Electrician before working on any electric.</p>
+                      <p className="text-right font-weight-lighter">* In no way is this meant to be construed as instructional procedures to installing Force, or performing work in an Electrical panel.</p>
+                      <p className="text-dark font-weight-bold">Parts: 2- Y-terminal (white), 2- Socket (yellow), 2- Wire, Device (FORCE), 2- Black rubber, 2- Cap, 4- 1 inch screw (Y-terminal and Socket are attached to wire)</p>
+                    <li> First locate your MAIN BREAKER, then TURN OFF THE MAIN BREAKER, Turn OFF All Breakers.
+                        <span className="text-danger">(**Please double-check with the electrical meter to ensure the electricity is off before removing the
+                        panel cover**)</span></li>
+                    <li>After double-checked the electricity is OFF, then carefully remove the panel cover.  </li>
+                    <li> Put Cap through Y-terminal and bring it towards to device to close (twist), make sure Socket is
+                    connected to the Force Device fully, and seal properly with Cap. </li>
+                    <li> Put Black robber (the narrow side facing Y-terminal) through Socket, then connect Socket to the Force Device until you hear "click". </li>
+                    <li> Put Cap through Y-terminal and bring it towards to device to close (twist), make sure Socket is connected to the Force Device fully, and seal properly with Cap. </li>
+                    <li> Put Black robber (the narrow side facing Y-terminal) through Socket, then connect Socket to the
+                    Force Device until you hear "click".</li>
+                    <li> Mount the FORCE device anywhere within 12 inches from the panel.</li>
+                    <li> Unscrew the bolt & nut that is attached to the bus bar, then connect one Y-terminal (white color) on the left side of the bus bar and screw the bolt & nut tight. </li>
+                    <li> Then follow procedure # 2-4 to connect another Y-terminal to the right side of the bus bar.  </li>
+                    <li>Safely install the panel cover back.   </li>
+                    <li> Turn on the main breaker. DONE!   </li>
                     </ul>
+               
+                    <Col lg="6"><CardBody>
+                    <CardImg alt="..."
+                      src={`${require("assets/img/theme/Installation 1 Image 1.png")}`}/>
+                  </CardBody></Col>
                   </TabPane>
+                
                   <TabPane tabId="plainTabs2">
                     <ul>
-                      <li>Open the door of distribution panel</li>
-                      <li>Check the main circuit breaker.</li>
-                      <li>Turn off the main circuit breaker.</li>
-                      <li>Check the location of each phase bus or secondary of the main circuit breaker.</li>
-                      <li>Connect the FORCE on each phase.</li>
-                      <li>Turn on the main circuit breaker.</li>
+                    <p className="text-danger text-right font-weight-bold">* Always consult a Licensed Electrician before working on any electric.</p>
+                    <p className="text-right font-weight-lighter">* In no way is this meant to be construed as instructional procedures to installing Force, or performing work in an Electrical panel.</p>
+                    <p className="text-dark font-weight-bold">Parts: 2- Y-terminal (white), 2- Socket (yellow), 2- Wire, Device (FORCE), 2- Black rubber, 2- Cap, 4- 1 inch screw (Y-terminal and Socket are attached to wire)</p>
+                    <li> First locate your MAIN BREAKER, then TURN OFF THE MAIN BREAKER, Turn OFF All Breakers.
+                    <span className="text-danger">(**Please double-check with the electrical meter to ensure the electricity is off before removing the panel cover**)</span></li>
+                    <li>  After double-checked the electricity is OFF, then carefully remove the panel cover. </li>
+                    <li> Put Black robber(the narrow side facing Y-terminal) through Socket, then connect Socket to the Force Device until you hear "click". </li>
+                    <li>Put Cap through Y-terminal and bring it towards to device to close(twist), make sure Socket is connected to the Force Device fully, and seal properly with Cap.</li>
+                    <li> Mount the FORCE device anywhere within 12 inches from the panel. </li>
+                    <li> Cut off Y-terminal from the wire, and peel about ½ inches from the tip of the wire cover (COVER ONLY).</li>
+                    <li>Connect wires to the switches. </li>
+                    <li>Remove the very first two switches from 1 or 2 side of the panel. </li>
+                    <li> Connect both of the FORCE attached switches on the top first two breakers of the panel that you have selected. </li>
+                    <li> After installing the FORCE switches on the panel you selected, turn ON switches ONLY! <span className="text-danger">(NOT A MAIN BREAKER)</span>.</li> 
+                    <li> Safely install the panel cover back. </li>
+                    <li> Turn ON the MAIN BREAKER. DONE! </li>
                     </ul>
                   </TabPane>
                 </TabContent>
+                </Col>
+                 </Row>
               </CardBody>
             </Card>
                   </Col>
