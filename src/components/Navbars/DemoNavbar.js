49d885bb6d9e50daeb05a8bbbae18e650bca905d
Venu Kommu <venu@baylogictech.com>
2021-05-20 16:41:48 +0530

    Merged search functionality into client review branch

--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -17,11 +17,7 @@
 */
 import React from "react";
 import { Link } from "react-router-dom";
-// JavaScript plugin that hides or shows a component based on your scroll
-//import Headroom from "headroom.js";
 import { CartContext } from "context/CartContext";
-//import { UserContext } from 'context/user';
-// reactstrap components
 import {
   UncontrolledCollapse,
   DropdownToggle,
@@ -39,7 +35,6 @@ import {
   DropdownItem
 } from "reactstrap";
 import App from "views/examples/App.js";
-import Auto from "views/examples/Auto.js";
 
 class DemoNavbar extends React.Component {
   static contextType = CartContext
@@ -114,103 +109,6 @@ class DemoNavbar extends React.Component {
                     </Col>
                   </Row>
                 </div>
-                {/* <Nav className="navbar-nav-hover align-items-lg-center" navbar>
-                  <UncontrolledDropdown nav>
-                    <DropdownToggle nav>
-                      <i className="ni ni-ui-04 d-lg-none mr-1" />
-                      <span className="nav-link-inner--text">Components</span>
-                    </DropdownToggle>
-                    <DropdownMenu className="dropdown-menu-xl">
-                      <div className="dropdown-menu-inner">
-                        <Media
-                          className="d-flex align-items-center"
-                          href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/overview?ref=adsr-navbar"
-                          target="_blank"
-                        >
-                          <div className="icon icon-shape bg-gradient-primary rounded-circle text-white">
-                            <i className="ni ni-spaceship" />
-                          </div>
-                          <Media body className="ml-3">
-                            <h6 className="heading text-primary mb-md-1">
-                              Getting started
-                            </h6>
-                            <p className="description d-none d-md-inline-block mb-0">
-                              Learn how to use Argon compiling Scss, change
-                              brand colors and more.
-                            </p>
-                          </Media>
-                        </Media>
-                        <Media
-                          className="d-flex align-items-center"
-                          href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/colors?ref=adsr-navbar"
-                          target="_blank"
-                        >
-                          <div className="icon icon-shape bg-gradient-success rounded-circle text-white">
-                            <i className="ni ni-palette" />
-                          </div>
-                          <Media body className="ml-3">
-                            <h6 className="heading text-primary mb-md-1">
-                              Foundation
-                            </h6>
-                            <p className="description d-none d-md-inline-block mb-0">
-                              Learn more about colors, typography, icons and the
-                              grid system we used for Argon.
-                            </p>
-                          </Media>
-                        </Media>
-                        <Media
-                          className="d-flex align-items-center"
-                          href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/alert?ref=adsr-navbar"
-                          target="_blank"
-                        >
-                          <div className="icon icon-shape bg-gradient-warning rounded-circle text-white">
-                            <i className="ni ni-ui-04" />
-                          </div>
-                          <Media body className="ml-3">
-                            <h5 className="heading text-warning mb-md-1">
-                              Components
-                            </h5>
-                            <p className="description d-none d-md-inline-block mb-0">
-                              Browse our 50 beautiful handcrafted components
-                              offered in the Free version.
-                            </p>
-                          </Media>
-                        </Media>
-                      </div>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                  <UncontrolledDropdown nav>
-                    <DropdownToggle nav>
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text">Examples</span>
-                    </DropdownToggle>
-                    <DropdownMenu>
-                      <DropdownItem to="/landing" tag={Link}>
-                        Landing
-                      </DropdownItem>
-                      <DropdownItem to="/profile" tag={Link}>
-                        Profile
-                      <DropdownItem to="/about" tag={Link}>
-                        About Company
-                      </DropdownItem>
-                      <DropdownItem to="/forceSystem" tag={Link}>
-                        Force System
-                      </DropdownItem>
-                      <DropdownItem to="/portfolio" tag={Link}>
-                        Project Portfolio
-                      </DropdownItem>
-                      <DropdownItem to="/download" tag={Link}>
-                        Download
-                      </DropdownItem>
-                      <DropdownItem to="/login" tag={Link}>
-                        Login
-                      </DropdownItem>
-                      <DropdownItem to="/register" tag={Link}>
-                        Register
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                </Nav> */}
                 <Nav className="navbar-nav-hover align-items-lg-center" navbar>
                   <UncontrolledDropdown nav>
                     <DropdownToggle to="#" tag={Link} nav >
@@ -257,38 +155,13 @@ class DemoNavbar extends React.Component {
                       <i className="ni ni-collection d-lg-none mr-1" />
                       <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }}>News</span>
                     </DropdownToggle>
-                    {/*} <DropdownMenu>
-                      <DropdownItem to="#" tag={Link}>
-                          Announcement
-                      </DropdownItem>
-                  </DropdownMenu>*/}
                   </UncontrolledDropdown>
                   <UncontrolledDropdown nav>
                     <DropdownToggle to="/clients" tag={Link} onClick={() => { window.location.href = "/clients" }} nav>
                       <i className="ni ni-collection d-lg-none mr-1" />
                       <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }} >Clients</span>
                     </DropdownToggle>
-                    {/*}<DropdownMenu>
-                      <DropdownItem to="/clients" tag={Link} onClick={() => {window.location.href="/clients"}}>
-                        Domestic
-                      </DropdownItem>
-                   <DropdownItem to="#" tag={Link}>
-                        Overseas
-                </DropdownItem>
-                    </DropdownMenu>*/}
                   </UncontrolledDropdown>
-                  {/*  <UncontrolledDropdown nav>
-                    <DropdownToggle to="/patents" tag={Link} nav>
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text">Patents</span>
-                    </DropdownToggle>
-                  </UncontrolledDropdown>
-                  <UncontrolledDropdown nav>
-                    <DropdownToggle to="#" tag={Link} nav>
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text">Certifications</span>
-                    </DropdownToggle>
-                </UncontrolledDropdown>  */}
                   <UncontrolledDropdown nav>
                     <DropdownToggle to="/store#!/Force/p/296981076/category=0" tag={Link} onClick={() => { window.location.href = "/store#!/Force/p/296981076/category=0" }} nav>
                       <i className="ni ni-collection d-lg-none mr-1" />
@@ -307,116 +180,11 @@ class DemoNavbar extends React.Component {
                       <DropdownItem to="/faq" tag={Link} onClick={() => { window.location.href = "/faq" }}>
                         FAQ
                       </DropdownItem>
-                      {/*<DropdownItem to="#" tag={Link}>
-                        Q&A
-                      </DropdownItem>*/}
                       <DropdownItem to="/contact" tag={Link} onClick={() => { window.location.href = "/contact" }}>
                         Contact Us
                       </DropdownItem>
                     </DropdownMenu>
                   </UncontrolledDropdown>
-                  {/*} <UncontrolledDropdown nav>
-                    <DropdownToggle to="/contact" tag={Link} nav>
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text">Contact</span>
-                    </DropdownToggle>
-              </UncontrolledDropdown> */}
-                  {/*<UncontrolledDropdown nav>
-                    <DropdownToggle to="/store" tag={Link}  onClick={() => {window.location.href="/store"}} nav>
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text">SHOP ENPOSS PRODUCTS</span>
-                    </DropdownToggle>
-                  </UncontrolledDropdown> 
-                  <NavItem>
-                    <NavLink to="/cart" tag={Link}
-                      className="nav-link-icon"
-                      //href="https://www.facebook.com/creativetim"
-                      //id="tooltip333589074"
-                      //target="_blank"
-                    >
-                      <i className="ni ni-cart" />
-                      <span >{qty}
-                      </span>
-                    </NavLink>
-                    {/*<UncontrolledTooltip delay={0} target="tooltip333589074">
-                      Like us on Facebook
-                      </UncontrolledTooltip>
-                  </NavItem>
-                  <UserContext.Consumer> 
-                  {({user,userLogout}) => ( 
-                    user.token ? (
-                    <UncontrolledDropdown nav>
-                      <DropdownToggle to="/" tag={Link} nav>
-                        <i className="ni ni-collection d-lg-none mr-1" />
-                        <span className="nav-link-inner--text" onClick={() => { userLogout(); }}>LOGOUT</span>
-                      </DropdownToggle>
-                    </UncontrolledDropdown>
-                    ) : '' )}
-                  </UserContext.Consumer>   */}
-                  {/* <NavItem>
-                    <NavLink
-                      className="nav-link-icon"
-                      href="https://www.facebook.com/creativetim"
-                      id="tooltip333589074"
-                      target="_blank"
-                    >
-                      <i className="fa fa-facebook-square" />
-                      <span className="nav-link-inner--text d-lg-none ml-2">
-                        Facebook
-                      </span>
-                    </NavLink>
-                    <UncontrolledTooltip delay={0} target="tooltip333589074">
-                      Like us on Facebook
-                    </UncontrolledTooltip>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className="nav-link-icon"
-                      href="https://www.instagram.com/creativetimofficial"
-                      id="tooltip356693867"
-                      target="_blank"
-                    >
-                      <i className="fa fa-instagram" />
-                      <span className="nav-link-inner--text d-lg-none ml-2">
-                        Instagram
-                      </span>
-                    </NavLink> 
-                    <UncontrolledTooltip delay={0} target="tooltip356693867">
-                      Follow us on Instagram
-                    </UncontrolledTooltip>
-                  </NavItem> 
-                  <NavItem>
-                    <NavLink
-                      className="nav-link-icon"
-                      href="https://twitter.com/creativetim"
-                      id="tooltip184698705"
-                      target="_blank"
-                    >
-                      <i className="fa fa-twitter-square" />
-                      <span className="nav-link-inner--text d-lg-none ml-2">
-                        Twitter
-                      </span>
-                    </NavLink>
-                    <UncontrolledTooltip delay={0} target="tooltip184698705">
-                      Follow us on Twitter
-                    </UncontrolledTooltip>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className="nav-link-icon"
-                      href="https://github.com/creativetimofficial/argon-design-system-react"
-                      id="tooltip112445449"
-                      target="_blank"
-                    >
-                      <i className="fa fa-github" />
-                      <span className="nav-link-inner--text d-lg-none ml-2">
-                        Github
-                      </span>
-                    </NavLink>
-                    <UncontrolledTooltip delay={0} target="tooltip112445449">
-                      Star us on Github
-                    </UncontrolledTooltip>
-                  </NavItem>*/}
                 </Nav>
                 <Nav className="ml-auto" navbar>
                   <NavItem className="d-none d-lg-block ml-xs-4" >
