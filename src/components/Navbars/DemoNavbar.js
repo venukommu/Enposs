20b9cde39d6c4a19a3f779e4a8e19df69a45ac27
madhulikagadang <gadangmadhulika@gmail.com>
2022-01-17 11:27:20 +0530

    overflow issue in shop page and flickering issue

--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -35,7 +35,7 @@ import {
   DropdownItem
 } from "reactstrap";
 import App from "views/examples/App.js";
-
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 class DemoNavbar extends React.Component {
   static contextType = CartContext
   componentDidMount() {
@@ -67,171 +67,175 @@ class DemoNavbar extends React.Component {
     //const {qty} = this.context;
     return (
       <>
-        <header className="header-global">
-          <Navbar
-            className="navbar-main navbar-transparent navbar-light bg-gradient-default"
-            expand="lg"
-            id="navbar-main"
-            style={{ position: "fixed" }}
-          >
-            <Container>
-              <NavbarBrand className="mr-lg-5" to="/" tag={Link} onClick={() => { window.location.href = "/" }}>
-                <img
-                  alt="..."
-                  src={require("assets/img/brand/EnpossLogo.png")}
-                />
-              </NavbarBrand>
-              <button className="navbar-toggler" id="navbar_global">
-                <span className="navbar-toggler-icon" />
-              </button>
-              <UncontrolledCollapse
-                toggler="#navbar_global"
-                navbar
-                className={this.state.collapseClasses}
-                onExiting={this.onExiting}
-                onExited={this.onExited}
+        <Router>
+          <Switch>
+            <header className="header-global">
+              <Navbar
+                className="navbar-main navbar-transparent navbar-light bg-gradient-default"
+                expand="lg"
+                id="navbar-main"
+                style={{ position: "fixed" }}
               >
-                <div className="navbar-collapse-header">
-                  <Row>
-                    <Col className="collapse-brand" xs="6">
-                      <Link to="/" onClick={() => { window.location.href = "/" }}>
-                        <img
-                          alt="..."
-                          src={require("assets/img/brand/EnpossLogo.png")}
-                        />
-                      </Link>
-                    </Col>
-                    <Col className="collapse-close" xs="6">
-                      <button className="navbar-toggler" id="navbar_global">
-                        <span />
-                        <span />
-                      </button>
-                    </Col>
-                  </Row>
-                </div>
-                <Nav className="navbar-nav-hover align-items-lg-center" navbar>
-                  <UncontrolledDropdown nav>
-                    <DropdownToggle to="#" tag={Link} nav >
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }}>About</span>
-                    </DropdownToggle>
-                    <DropdownMenu>
-                      <DropdownItem to="/ourstory" onClick={() => { window.location.href = "/ourstory" }} tag={Link}>
-                        Our Story
+                <Container>
+                  <NavbarBrand className="mr-lg-5" to="/" tag={Link} onClick={() => { window.location.href = "/" }}>
+                    <img
+                      alt="..."
+                      src={require("assets/img/brand/EnpossLogo.png")}
+                    />
+                  </NavbarBrand>
+                  <button className="navbar-toggler" id="navbar_global">
+                    <span className="navbar-toggler-icon" />
+                  </button>
+                  <UncontrolledCollapse
+                    toggler="#navbar_global"
+                    navbar
+                    className={this.state.collapseClasses}
+                    onExiting={this.onExiting}
+                    onExited={this.onExited}
+                  >
+                    <div className="navbar-collapse-header">
+                      <Row>
+                        <Col className="collapse-brand" xs="6">
+                          <Link to="/" onClick={() => { window.location.href = "/" }}>
+                            <img
+                              alt="..."
+                              src={require("assets/img/brand/EnpossLogo.png")}
+                            />
+                          </Link>
+                        </Col>
+                        <Col className="collapse-close" xs="6">
+                          <button className="navbar-toggler" id="navbar_global">
+                            <span />
+                            <span />
+                          </button>
+                        </Col>
+                      </Row>
+                    </div>
+                    <Nav className="navbar-nav-hover align-items-lg-center" navbar>
+                      <UncontrolledDropdown nav>
+                        <DropdownToggle to="#" tag={Link} nav >
+                          <i className="ni ni-collection d-lg-none mr-1" />
+                          <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }}>About</span>
+                        </DropdownToggle>
+                        <DropdownMenu>
+                          <DropdownItem to="/ourstory" onClick={() => { window.location.href = "/ourstory" }} tag={Link}>
+                            Our Story
                       </DropdownItem>
-                      <DropdownItem to="/ceomessage" onClick={() => { window.location.href = "/ceomessage" }} tag={Link}>
-                        CEO Message
+                          <DropdownItem to="/ceomessage" onClick={() => { window.location.href = "/ceomessage" }} tag={Link}>
+                            CEO Message
                       </DropdownItem>
-                      <DropdownItem to="/timeline" onClick={() => { window.location.href = "/timeline" }} tag={Link}>
-                        Timeline
+                          <DropdownItem to="/timeline" onClick={() => { window.location.href = "/timeline" }} tag={Link}>
+                            Timeline
                       </DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                  <UncontrolledDropdown nav>
-                    <DropdownToggle to="#" tag={Link} nav >
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }}>Product</span>
-                    </DropdownToggle>
-                    <DropdownMenu>
-                      <DropdownItem to="/force" tag={Link} onClick={() => { window.location.href = "/force" }}>
-                        What is FORCE?
+                        </DropdownMenu>
+                      </UncontrolledDropdown>
+                      <UncontrolledDropdown nav>
+                        <DropdownToggle to="#" tag={Link} nav >
+                          <i className="ni ni-collection d-lg-none mr-1" />
+                          <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }}>Product</span>
+                        </DropdownToggle>
+                        <DropdownMenu>
+                          <DropdownItem to="/force" tag={Link} onClick={() => { window.location.href = "/force" }}>
+                            What is FORCE?
                       </DropdownItem>
-                      <DropdownItem to="/benefits" tag={Link} onClick={() => { window.location.href = "/benefits" }}>
-                        Benefits of FORCE
+                          <DropdownItem to="/benefits" tag={Link} onClick={() => { window.location.href = "/benefits" }}>
+                            Benefits of FORCE
                       </DropdownItem>
-                      <DropdownItem to="/forceprinciples" tag={Link} onClick={() => { window.location.href = "/forceprinciples" }}>
-                        Working Principle
+                          <DropdownItem to="/forceprinciples" tag={Link} onClick={() => { window.location.href = "/forceprinciples" }}>
+                            Working Principle
                       </DropdownItem>
-                      <DropdownItem to="/patents" tag={Link} onClick={() => { window.location.href = "/patents" }}>
-                        Patents
+                          <DropdownItem to="/patents" tag={Link} onClick={() => { window.location.href = "/patents" }}>
+                            Patents
                       </DropdownItem>
-                      <DropdownItem to="/certifications" tag={Link} onClick={() => { window.location.href = "/certifications" }}>
-                        Certifications
+                          <DropdownItem to="/certifications" tag={Link} onClick={() => { window.location.href = "/certifications" }}>
+                            Certifications
                       </DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                  <UncontrolledDropdown nav>
-                    <DropdownToggle to="/news" tag={Link} nav onClick={() => { window.location.href = "/news" }}>
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }}>News</span>
-                    </DropdownToggle>
-                  </UncontrolledDropdown>
-                  <UncontrolledDropdown nav>
-                    <DropdownToggle to="/clients" tag={Link} onClick={() => { window.location.href = "/clients" }} nav>
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }} >Clients</span>
-                    </DropdownToggle>
-                  </UncontrolledDropdown>
-                  <UncontrolledDropdown nav>
-                    <DropdownToggle to="/store#!/Force/p/296981076/category=0" tag={Link} onClick={() => { window.location.href = "/store#!/Force/p/296981076/category=0" }} nav>
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }}>Shop</span>
-                    </DropdownToggle>
-                  </UncontrolledDropdown>
-                  <UncontrolledDropdown nav>
-                    <DropdownToggle to="#" tag={Link} nav>
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }}>Support</span>
-                    </DropdownToggle>
-                    <DropdownMenu>
-                      <DropdownItem to="/installforce" tag={Link} onClick={() => { window.location.href = "/installforce" }}>
-                        How to install FORCE
+                        </DropdownMenu>
+                      </UncontrolledDropdown>
+                      <UncontrolledDropdown nav>
+                        <DropdownToggle to="/news" tag={Link} nav onClick={() => { window.location.href = "/news" }}>
+                          <i className="ni ni-collection d-lg-none mr-1" />
+                          <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }}>News</span>
+                        </DropdownToggle>
+                      </UncontrolledDropdown>
+                      <UncontrolledDropdown nav>
+                        <DropdownToggle to="/clients" tag={Link} onClick={() => { window.location.href = "/clients" }} nav>
+                          <i className="ni ni-collection d-lg-none mr-1" />
+                          <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }} >Clients</span>
+                        </DropdownToggle>
+                      </UncontrolledDropdown>
+                      <UncontrolledDropdown nav>
+                        <DropdownToggle to="/store#!/Force/p/296981076/category=0" tag={Link} onClick={() => { window.location.href = "/store#!/Force/p/296981076/category=0" }} nav>
+                          <i className="ni ni-collection d-lg-none mr-1" />
+                          <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }}>Shop</span>
+                        </DropdownToggle>
+                      </UncontrolledDropdown>
+                      <UncontrolledDropdown nav>
+                        <DropdownToggle to="#" tag={Link} nav>
+                          <i className="ni ni-collection d-lg-none mr-1" />
+                          <span className="nav-link-inner--text text-uppercase font-weight-bold" style={{ fontSize: "17px" }}>Support</span>
+                        </DropdownToggle>
+                        <DropdownMenu>
+                          <DropdownItem to="/installforce" tag={Link} onClick={() => { window.location.href = "/installforce" }}>
+                            How to install FORCE
                       </DropdownItem>
-                      <DropdownItem to="/faq" tag={Link} onClick={() => { window.location.href = "/faq" }}>
-                        FAQ
+                          <DropdownItem to="/faq" tag={Link} onClick={() => { window.location.href = "/faq" }}>
+                            FAQ
                       </DropdownItem>
-                      <DropdownItem to="/contact" tag={Link} onClick={() => { window.location.href = "/contact" }}>
-                        Contact Us
+                          <DropdownItem to="/contact" tag={Link} onClick={() => { window.location.href = "/contact" }}>
+                            Contact Us
                       </DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                </Nav>
-                <Nav className="ml-auto" navbar>
-                  <NavItem className="d-none d-lg-block ml-xs-4" >
-                    <NavLink
-                      className="nav-link-icon"
-                      href="http://www.enposs.kr/"
-                      id="tooltip112445449"
-                      target="_blank"
-                      title="Korean Site"
-                    >
-                      <img src={require("assets/img/flags/f_kr.png")} height="18" alt="Korean Site" /> </NavLink></NavItem>
-                  <NavItem className="d-none d-lg-block ml-xs-4" >
-                    <NavLink
-                      className="nav-link-icon"
-                      href="http://www.enposs.jp/"
-                      id="tooltip112445449"
-                      target="_blank"
-                      title="Japanese Site"
-                    >
-                      <img src={require("assets/img/flags/f_jp.png")} height="18" alt="Japanese Site" /> </NavLink></NavItem>
-                  <NavItem className="d-none d-lg-block ml-xs-4" >
-                    <NavLink
-                      className="nav-link-icon"
-                      href="http://www.enposs.com.cn/"
-                      id="tooltip112445449"
-                      target="_blank"
-                      title="Chinese Site"
-                    >
-                      <img src={require("assets/img/flags/f_ch.png")} height="18" alt="Chinese Site" /> </NavLink></NavItem>
-                  <NavItem className="d-none d-lg-block ml-xs-4" >
-                    <NavLink
-                      className="nav-link-icon"
-                      href="http://enposs.vn/"
-                      id="tooltip112445449"
-                      target="_blank"
-                      title="Vietnamese Site"
-                    >
-                      <img src={require("assets/img/flags/f_vt.png")} height="18" alt="Vietnamese Site" /> </NavLink></NavItem>
-                  {/*((currentURL === "https://www.enposs.com/store#!/Force/p/296981076/category=0") || (currentURL !== "http://localhost:3000/store#!/Force/p/296981076/category=0")) ?*/}
-                  {/* (pathname === "/")?*/}
-                    <App indexName="searchdata" />
+                        </DropdownMenu>
+                      </UncontrolledDropdown>
+                    </Nav>
+                    <Nav className="ml-auto" navbar>
+                      <NavItem className="d-none d-lg-block ml-xs-4" >
+                        <NavLink
+                          className="nav-link-icon"
+                          href="http://www.enposs.kr/"
+                          id="tooltip112445449"
+                          target="_blank"
+                          title="Korean Site"
+                        >
+                          <img src={require("assets/img/flags/f_kr.png")} height="18" alt="Korean Site" /> </NavLink></NavItem>
+                      <NavItem className="d-none d-lg-block ml-xs-4" >
+                        <NavLink
+                          className="nav-link-icon"
+                          href="http://www.enposs.jp/"
+                          id="tooltip112445449"
+                          target="_blank"
+                          title="Japanese Site"
+                        >
+                          <img src={require("assets/img/flags/f_jp.png")} height="18" alt="Japanese Site" /> </NavLink></NavItem>
+                      <NavItem className="d-none d-lg-block ml-xs-4" >
+                        <NavLink
+                          className="nav-link-icon"
+                          href="http://www.enposs.com.cn/"
+                          id="tooltip112445449"
+                          target="_blank"
+                          title="Chinese Site"
+                        >
+                          <img src={require("assets/img/flags/f_ch.png")} height="18" alt="Chinese Site" /> </NavLink></NavItem>
+                      <NavItem className="d-none d-lg-block ml-xs-4" >
+                        <NavLink
+                          className="nav-link-icon"
+                          href="http://enposs.vn/"
+                          id="tooltip112445449"
+                          target="_blank"
+                          title="Vietnamese Site"
+                        >
+                          <img src={require("assets/img/flags/f_vt.png")} height="18" alt="Vietnamese Site" /> </NavLink></NavItem>
+                      {/*((currentURL === "https://www.enposs.com/store#!/Force/p/296981076/category=0") || (currentURL !== "http://localhost:3000/store#!/Force/p/296981076/category=0")) ?*/}
+                      {/* (pathname === "/")?*/}
+                      <App indexName="searchdata" />
 
-                </Nav>
-              </UncontrolledCollapse>
-            </Container>
-          </Navbar>
-        </header>
+                    </Nav>
+                  </UncontrolledCollapse>
+                </Container>
+              </Navbar>
+            </header>
+          </Switch>
+        </Router>
 
       </>
     );
